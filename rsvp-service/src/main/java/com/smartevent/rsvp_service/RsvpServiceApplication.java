340c69ec7c9853ec84d2d5e8fc8c44742305ee42
rakesh <rakeshperam2@gmail.com>
2025-11-05 16:11:54 +0530

    Added rsvp service microservice

---
+++ rsvp-service/src/main/java/com/smartevent/rsvp_service/RsvpServiceApplication.java
@@ -0,0 +1,13 @@
+package com.smartevent.rsvp_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsvpServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsvpServiceApplication.class, args);
+	}
+
+}
